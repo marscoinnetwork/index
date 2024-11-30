f6e077aadf18e311c3f432c8f33dfa2889dc0605
young <sealink272@gmail.com>
2024-11-30 15:08:53 +0800

    Update index core models

---
+++ Blockcore.Index.Core/Models/Reward.cs
@@ -0,0 +1,9 @@
+namespace Blockcore.Index.Core.Models
+{
+   public class RewardModel
+   {
+      public long Height { get; set; }
+
+      public long Reward { get; set; }
+   }
+}
---
+++ Blockcore.Index.Core/Models/StatsConnection.cs
@@ -0,0 +1,10 @@
+namespace Blockcore.Index.Core.Models
+{
+   public class StatsConnection
+   {
+      /// <summary>
+      /// Gets or sets the Connections.
+      /// </summary>
+      public int Connections { get; set; }
+   }
+}
