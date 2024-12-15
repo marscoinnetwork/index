09ab421de6aaeccfb39448a8b3f1fd7dc7715d3b
young <sealink272@gmail.com>
2024-12-15 21:45:15 +0800

    Add index core client

--- XOuranos.Index.Core/Client/ICryptoClientFactory.cs
+++
@@ -1,9 +0,0 @@
-using XOuranos.Index.Core.Operations.Types;
-
-namespace XOuranos.Index.Core.Client
-{
-   public interface ICryptoClientFactory
-   {
-      IBlockchainClient Create(SyncConnection connection);
-   }
-}
